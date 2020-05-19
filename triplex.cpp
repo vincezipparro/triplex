4b8fa07753ab800599849308a1cf1a2258bc5948
Vince Zipparro <vincezipparro@gmail.com>
2020-05-18 21:41:09 -0500

    declare max levels

--- triplex.cpp
+++ triplex.cpp
@@ -7,8 +7,9 @@ int main()
 {
     Game Execute;
     int LevelDifficulty = 1;
+    const int maxDifficulty = 10;
 
-    while(true)
+    while(true) // Loop game till all levels are completely
     {
         // Execute.PrintIntroduction(7);
         bool bLevelComplete = Execute.PlayGame(LevelDifficulty);
